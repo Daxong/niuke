b0b6b60d7a27e9c59b8a6abebb491ddcf49d48a0
aFang <daxong.y@gmail.com>
2018-08-24 09:48:55 +0800

    替换输入函数nextline->next

--- src/niuke/CycleVoca.java
+++ src/niuke/CycleVoca.java
@@ -13,12 +13,17 @@ public class CycleVoca {
         }else{
             return;
         }
-
+//        in.nextLine();
         for (int i = 0 ; i < vocabularys.length ; i++) {
-            String str = in.nextLine();
+
+            String str = in.next();
+
             if (str.length() >= 1 & str.length() <= 50) {
                 vocabularys[i] = str;
             }else{
+                return;
+            }
+            if (str.matches("/^[A-Z]/g")) {
                 return;
             }
         }
@@ -30,18 +35,26 @@ public class CycleVoca {
             }
             String ss = vocabularys[i]+vocabularys[i];
             Set set = new HashSet<Integer>();
-
-            for (int n = 0 ; n < vocabularys.length ; n++) {
+            int flag = 0;
+            for (int n = i ; n < vocabularys.length ; n++) {
                 if (count.contains(i)){
                     continue;
                 }
-                if (ss.contains(vocabularys[n])) {
-                    set.add(i);
-                    set.add(n);
-                    map.put(ss,set);
+                if (n+1 < vocabularys.length) {
+                    if (ss.contains(vocabularys[n+1])) {
+                        flag = 1;
+                        set.add(i);
+                        set.add(n);
+                        map.put(ss,set);
+                    }
                 }
+
             }
-            count.addAll(set);
+
+            if (flag == 1) {
+                count.addAll(set);
+            }
+
         }
 
         System.out.print(map.size());
