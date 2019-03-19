a4024471615942a363ff2320aac9e0ec2cbdbb41
aFang <daxong.y@gmail.com>
2019-03-19 09:55:22 +0800

    leetcode sunNumbers

---
+++ src/lee/leetcode102/SumNumbers.java
@@ -0,0 +1,39 @@
+package lee.leetcode102;
+
+import java.util.LinkedList;
+
+public class SumNumbers {
+
+    private LinkedList<String> list = new LinkedList();
+    public static void main(String[] args) {
+        new SumNumbers().sumNumbers();
+
+    }
+    public void sumNumbers() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        String integer = "";
+        inorder(root, integer);
+    }
+    private void inorder(TreeNode root, String integer){
+        if (root != null) {
+            integer += root.val+"";
+            inorder(root.left, integer);
+            inorder(root.right,integer);
+            if (root.right == null && root.left == null) {
+                list.add(integer);
+                System.out.println(integer);
+            }
+            
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+}
