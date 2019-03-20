4f093ec2918eb35b4280fd526e0aaf917fcb470a
aFang <daxong.y@gmail.com>
2019-03-20 16:34:53 +0800

    OK Coin笔试算法题

---
+++ src/lee/leetcode102/ClimbStair.java
@@ -0,0 +1,26 @@
+package lee.leetcode102;
+
+public class ClimbStair {
+
+    public static void main(String[] args) {
+
+        System.out.println(stair(3));
+    }
+
+    public static int stair(int n) {
+        int sum = 0;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else if (n > 2) {
+            return stair(n - 2) + stair(n - 1);
+        }
+
+        return sum;
+    }
+}
+
---
+++ src/lee/leetcode102/compress.java
@@ -0,0 +1,34 @@
+package lee.leetcode102;
+
+import java.util.Scanner;
+
+public class compress {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String str =scanner.nextLine();
+        System.out.println(compress(str));
+    }
+
+    public static String compress(String str){
+        char prev = str.charAt(0);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(prev);
+        int count = 1;
+        for (int i = 1; i < str.length(); i++) {
+            char cur = str.charAt(i);
+            if (cur == prev){
+                count++;
+            }else {
+                if (count!=1) stringBuilder.append(count);
+                count = 1;
+                stringBuilder.append(cur);
+                prev = cur;
+            }
+        }
+        if (count!=1) stringBuilder.append(count);
+        return stringBuilder.toString();
+
+    }
+}
