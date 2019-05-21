40281ee3b43670def00b505cad00eb341c105c7d
aFang <daxong.y@gmail.com>
2019-05-21 21:32:07 +0800

    求解数独

---
+++ src/leetcode/SolveSudoku.java
@@ -0,0 +1,105 @@
+package leetcode;
+
+/**
+ * 编写一个程序，通过已填充的空格来解决数独问题。
+ *
+ * 一个数独的解法需遵循如下规则：
+ *
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ * 空白格用 '.' 表示。
+ */
+public class SolveSudoku {
+    /**
+     * 输入：一个二维数组 代表当前九宫格值
+     * @param board
+     */
+    public static void solveSudoku(char[][] board){
+
+        /**
+         * 记录某行，某位数字是否已经被摆放
+         */
+        boolean[][] row = new boolean[9][10];
+        /**
+         * 记录某列，某位数字是否已经被摆放
+         */
+        boolean[][] col = new boolean[9][10];
+        /**
+         * 记录某 3x3 宫格内，某位数字是否已经被摆放
+         */
+        boolean[][] block = new boolean[9][10];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    row[i][num] = true;
+                    col[j][num] = true;
+                    // blockIndex = i / 3 * 3 + j / 3，取整
+                    block[i / 3 * 3 + j / 3][num] = true;
+                }
+            }
+        }
+        dfs(board, row, col, block, 0, 0);
+    }
+
+    private static boolean dfs(char[][] board, boolean[][] row, boolean[][] col, boolean[][] block, int i, int j) {
+        // 找寻空位置
+        while (board[i][j] != '.') {
+            if (++j >= 9) {
+                i++;
+                j = 0;
+            }
+            if (i >= 9) {
+                return true;
+            }
+        }
+        for (int num = 1; num <= 9; num++) {
+            int blockIndex = i / 3 * 3 + j / 3;
+            if (!row[i][num] && !col[j][num] && !block[blockIndex][num]) {
+                // 递归
+                board[i][j] = (char) ('0' + num);
+                row[i][num] = true;
+                col[j][num] = true;
+                block[blockIndex][num] = true;
+                if (dfs(board, row, col, block, i, j)) {
+                    return true;
+                } else {
+                    // 回溯
+                    row[i][num] = false;
+                    col[j][num] = false;
+                    block[blockIndex][num] = false;
+                    board[i][j] = '.';
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void printBoard(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+//        printBoard(board);
+        solveSudoku(board);
+        printBoard(board);
+    }
+}
