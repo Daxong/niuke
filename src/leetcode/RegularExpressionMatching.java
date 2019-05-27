c63b2893dea3797b23cc5437668957276846a855
aFang <daxong.y@gmail.com>
2019-05-27 21:26:21 +0800

    正则表达式匹配 dp解法

---
+++ src/leetcode/RegularExpressionMatching.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+/**
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ *
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ *
+ * Note:
+ *
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ */
+public class RegularExpressionMatching {
+
+    public static boolean isMatch(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+        boolean[][] memory = new boolean[2][pLen+1];
+        memory[0][0] = true;
+        int cur = 0, pre = 0;
+        for(int i = 0; i <= sLen; i++) {
+            cur = i % 2;
+            pre = (i + 1) % 2;
+            if(i > 1) {
+                for(int j = 0; j <= pLen; j++) {
+                    memory[cur][j] = false;
+                }
+            }
+            for(int j = 1; j <= pLen; j++) {
+                if(p.charAt(j-1) == '*') {
+                    memory[cur][j] = memory[cur][j-2] || (i > 0 && (s.charAt(i-1) == p.charAt(j-2) ||
+                            p.charAt(j-2) == '.') && memory[pre][j]);
+                }else {
+                    memory[cur][j] = i > 0 && (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.')
+                            && memory[pre][j-1];
+                }
+            }
+        }
+        return memory[cur][pLen];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
